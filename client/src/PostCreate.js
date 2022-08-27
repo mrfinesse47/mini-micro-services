ef0c61340df595ab840fa88d0c2751e3f0752138
Kevin Mason <krmason2020@gmail.com>
2022-08-26 20:16:03 -0700

    updates to client

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const PostCreate = () => {
-  const [title, setTitle] = useState('');
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    await axios.post('http://localhost:4000/posts', { title });
-    setTitle('');
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor=''>Title</label>
+        <div className="form-group">
+          <label>Title</label>
           <input
-            type='text'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className='form-control'
+            className="form-control"
           />
         </div>
-        <button className='btn btn-primary'>Submit</button>
+        <button className="btn btn-primary">Submit</button>
       </form>
     </div>
   );
